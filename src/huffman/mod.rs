13d8b4c3c5b0361f335771f7b8229f5dd33eb7b7
Thomas Pickert <thpickert@ennit.de>
2015-09-27 17:49:35 -0400

    MOD: generated proper bit strings from code lengths

--- src/huffman/mod.rs
+++ src/huffman/mod.rs
@@ -1,4 +1,21 @@
-pub fn codes_from_lengths(ref lengths: Vec<usize>) -> Vec<usize> {
+fn bit_string_from_code_and_length(code: usize, len: usize) -> Vec<bool> {
+	let mut bits = vec![false; len];
+
+	for i in 0..len {
+		bits[len - i - 1] = (code >> i) & 1 == 1;
+	}
+
+	bits
+}
+
+#[test]
+fn should_honor_leading_zeroes() {
+	assert_eq!(vec![false, false], bit_string_from_code_and_length(0b00, 2));
+	assert_eq!(vec![false, true, true], bit_string_from_code_and_length(0b011, 3));
+}
+
+
+pub fn codes_from_lengths(ref lengths: Vec<usize>) -> Vec<Vec<bool>> {
 	let max_length = lengths.iter().fold(0, |acc, &len| if len > acc { len } else { acc });
 	let mut bl_count = vec![0; max_length + 1];
 	for &len in lengths {
@@ -12,11 +29,11 @@ pub fn codes_from_lengths(ref lengths: Vec<usize>) -> Vec<usize> {
 		next_code[bits] = code;
 	}
 
-	let mut codes = vec![0; lengths.len()];
+	let mut codes = vec![vec![]; lengths.len()];
 	for i in 0..codes.len() {
 		let len = lengths[i];
 		if len > 0 {
-			codes[i] = next_code[len];
+			codes[i] = bit_string_from_code_and_length(next_code[len], len);
 			next_code[len] += 1;
 		}
 	}
@@ -33,7 +50,7 @@ mod tests {
 		let lengths = vec![1];
 		let codes = codes_from_lengths(lengths);
 
-		assert_eq!(vec![0], codes);
+		assert_eq!(vec![vec![false]], codes);
 	}
 
 	#[test]
@@ -43,6 +60,6 @@ mod tests {
 		let lengths = vec![3, 3, 3, 3, 3, 2, 4, 4];
 		let codes = codes_from_lengths(lengths);
 
-		assert_eq!(vec![0b010, 0b011, 0b100, 0b101, 0b110, 0b00, 0b1110, 0b1111], codes);
+		assert_eq!(vec![vec![false, true, false], vec![false, true, true], vec![true, false, false], vec![true, false, true], vec![true, true, false], vec![false, false], vec![true, true, true, false], vec![true, true, true, true]], codes);
 	}
 }
