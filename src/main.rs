e9094efee4441b98863d0441657563f34dfff548
Thomas Pickert <thpickert@ennit.de>
2015-10-21 16:51:17 -0400

    MOD: used RingBuffer for sliding output window, commented out all debug lines

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 extern crate compression;
 
 fn main() {
-	use std::io::{ Read };
+	use std::io::{ Read, stdout, Write };
 	use compression::brotli::Decompressor;
 	use compression::bitreader::BitReader;
 
-	let brotli_stream = BitReader::new(std::fs::File::open("data/asyoulik.txt.compressed").unwrap());
+	let brotli_stream = BitReader::new(std::fs::File::open("data/plrabn12.txt.compressed").unwrap());
 
 	let mut decompressed = &mut Vec::new();
 	let _ = Decompressor::new(brotli_stream).read_to_end(&mut decompressed);
 
 	let mut expected = &mut Vec::new();
-	let _ = std::fs::File::open("data/asyoulik.txt").unwrap().read_to_end(&mut expected);
+	let _ = std::fs::File::open("data/plrabn12.txt").unwrap().read_to_end(&mut expected);
 
 	assert_eq!(expected, decompressed);
+
+	// stdout().write_all(decompressed);
 }
 
